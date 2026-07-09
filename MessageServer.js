a89fb67f08b16c71150686a6af2ef16461122bf8
itsikram <mdikram295@gmail.com>
2026-07-09 07:33:45 +0600

    fix the auth

--- MessageServer.js
+++ MessageServer.js
@@ -1848,7 +1848,7 @@ export class MessageServer extends EventEmitter {
       // CORS headers
       res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, HEAD');
-      res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
       
       if (req.method === 'OPTIONS') {
         res.writeHead(200);
